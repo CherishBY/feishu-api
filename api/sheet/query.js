b5830cfe5fe878b6aa2d234ff757dddaeabb4472
huaby <3012037233@qq.com>
2025-02-15 18:29:56 +0800

    添加表格查询接口

---
+++ api/sheet/query.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export async (req, res) => {
+	try {
+		console.log(req.headers);
+		// 接口 headers
+		const accessToken = req.headers['Authorization'];
+		// 接口 参数
+		const range = req.query.range;
+		const spreadSheetToken = req.query.spreadSheetToken
+		
+		const response = await axios.get(`https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadSheetToken}/sheets/query`, {
+			headers: {
+				'Authorization': `Bearer ${accessToken}`
+			}
+		});
+		
+		res.status(200).json(response.data);
+	} catch(error) {
+		console.log('error res', res);
+		console.log('error', error);
+		console.log('error status', res.status);
+		res.status(500).json({error: error.message});
+	}
+}
