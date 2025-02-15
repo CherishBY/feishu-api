d8dadfc020797c0eb8770eeef1ca00c65ff80e4a
huaby <3012037233@qq.com>
2025-02-15 19:02:21 +0800

    测试

--- api/query.js
+++ api/query.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
 
-export async (req, res) => {
-	try {
+module.exports = async (req, res) => {
+    try {
         const accessToken = req.query.accessToken;
-		const spreadSheetToken = req.query.spreadSheetToken
-		
-		const response = await axios.get(`https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadSheetToken}/sheets/query`, {
-			mode: 'no-cors',
-			headers: {
-				'Authorization': `Bearer ${accessToken}`
-			}
-		});
-		
-		res.status(200).json(response.data);
-	} catch(error) {
-		res.status(500).json({error: error.message});
-	}
-}
+        const spreadsheetToken = 'Tx0bsaPXFhVbvqtKhSKcxhjdnue'; // 替换为你的电子表格 Token
+
+        const response = await axios.get(
+			`https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadsheetToken}/sheets/query`,
+            {
+				mode:'no-cors',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            }
+        );
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
