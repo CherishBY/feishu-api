41e13e7afe60bd33b7e5bc2c820467a9ae08bfff
huaby <3012037233@qq.com>
2025-02-16 17:55:33 +0800

    修改请求方式

--- api/tenant_access_token.js
+++ api/tenant_access_token.js
@@ -6,9 +6,8 @@ module.exports = async (req, res) => {
     const appID = req.query.app_id;
     const appSecret = req.query.app_secret;
 
-    const response = await axios.get(`https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/`,
+    const response = await axios.post(`https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/`,
       {
-        method: 'POST',
         mode: 'no-cors',
         data: JSON.stringify({
           app_id: appID,
