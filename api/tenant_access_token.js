47e14beab5728c6be70c146b3aba14473f082cc7
huaby <3012037233@qq.com>
2025-02-16 19:05:19 +0800

    test

--- api/tenant_access_token.js
+++ api/tenant_access_token.js
@@ -3,17 +3,17 @@ const axios = require('axios');
 module.exports = async (req, res) => {
   try {
     const data = {
-      api_id: 'cli_a726d69f89b15013',
-      api_secret: 'IYWLrVKqguwwI8tMStpWgeIzgjkFZnsw',
+      app_id: 'cli_a726d69f89b15013',
+      app_secret: 'IYWLrVKqguwwI8tMStpWgeIzgjkFZnsw',
     };
 
     const response = await axios.post('https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/',
-      data,
       {
         mode: 'no-cors',
         headers: {
           'Content-Type': 'application/json'
-        }
+        },
+        data: data
       }
     );
 
