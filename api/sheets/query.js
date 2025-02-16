cd841bf091f4ea5cf6d71a8d25ce4f4da077e848
huaby <3012037233@qq.com>
2025-02-16 23:20:46 +0800

    test

--- api/sheets/query.js
+++ api/sheets/query.js
@@ -9,13 +9,18 @@ module.exports = async (req, res) => {
 
         // const params = req.queryStringParameters || {};
 
-        const response = await axios.get(
-			`https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadsheetToken}/sheets/query`,
-            {
-				mode:'no-cors',
-                headers: headers
-            }
-        );
+        // const response = await axios.get(`https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadsheetToken}/sheets/query`,
+        //     {
+		// 		mode:'no-cors',
+        //         headers: headers
+        //     }
+        // );
+        const response = await axios({
+            url: `https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadsheetToken}/sheets/query`,
+            method: 'GET',
+            headers: headers,
+            mode: 'no-cors'
+        });
 
         res.status(200).json(response.data);
     } catch (error) {
