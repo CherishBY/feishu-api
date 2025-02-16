6be3f14e37d5a747b1d49cf223ee23975e2c07cd
huaby <3012037233@qq.com>
2025-02-16 23:24:24 +0800

    test

--- api/sheets/query.js
+++ api/sheets/query.js
@@ -7,21 +7,12 @@ module.exports = async (req, res) => {
         const accessToken = req.query.accessToken;
         const spreadsheetToken = req.query.spreadsheetToken; // 电子表格 Token
 
-        // const params = req.queryStringParameters || {};
-
-        // const response = await axios.get(`https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadsheetToken}/sheets/query`,
-        //     {
-        // 		mode:'no-cors',
-        //         headers: headers
-        //     }
-        // );
         const response = await axios({
             url: `https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/${spreadsheetToken}/sheets/query`,
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${accessToken}`
-            },
-            mode: 'no-cors'
+            }
         });
 
         res.status(200).json(response.data);
